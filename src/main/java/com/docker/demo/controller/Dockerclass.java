794920238c43dbaebd746a820bbb171b3790ffb3
saikumar <20r01a05b4@cmritonline.ac.in>
2026-09-04 11:34:06 +0530

    added all the configuration

---
+++ src/main/java/com/docker/demo/controller/Dockerclass.java
@@ -0,0 +1,30 @@
+package com.docker.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Dockerclass {
+    @Autowired
+	private repo r;
+	@RequestMapping("/fetch")
+	public List<Student> display() {
+		return r.findAll();
+	}
+	@RequestMapping("/")
+    public void add() {
+		List<Student> l=new ArrayList<>();
+		 Student s=new Student();
+		 //s.setId(1);
+		 s.setName("sai");
+		 Student s1=new Student();
+		// s1.setId(2);
+		 s1.setName("ram");
+		 l.add(s1);
+         r.saveAll(l);	
+    }
+}
